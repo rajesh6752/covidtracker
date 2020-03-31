d2a9ca0c81db9da5e6e3d721fda76e01a12f898e
rajesh6752 <41473204+rajesh6752@users.noreply.github.com>
2020-03-31 10:41:27 +0530

    UPDATED SCRIPT.JS FILE

--- script.js
+++ script.js
@@ -89,14 +89,14 @@ function constructTweetButton() {
     // let current_date = new Date().toLocaleString('en-IN');
     let active_delta = key_values.confirmeddelta - key_values.recovereddelta
     let active_delta_status = active_delta >= 0? "increased" : "decreased";
-    const tweet_content = `COVID-19 India : 📊 as of ${current_date} IST
+    const tweet_content = `CORONA VIRUS IN India : 📊 as of ${current_date} IST
     Total Confirmed : ${total.confirmed}
     Total Recovered : ${total.recovered}
     Total Deceased  : ${total.deaths}
 
     Number of cases reported today: ${Math.abs(key_values.confirmeddelta)}
 
-    Follow @covid19indiaorg
+    Follow @rajesh_shukla
 
     #COVID19India #SocialDistancing
     More @`
